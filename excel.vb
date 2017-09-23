f53df02d7c97f0f5afd3230f14b133a1d6ba7945
rocharpe <31942299+rocharpe@users.noreply.github.com>
2017-09-23 10:44:35 +0800

    Update excel.vb

--- excel.vb
+++ excel.vb
@@ -8,3 +8,15 @@
 '第二格：输入公式 
 '（其中sheet2是数据库，$A:$A为数据库所在位置，A1是开始选中列的起时位置）
   =countif(sheet2!$A:$A,A1)=1
+
+  '表格多列统计到一列宏代码，大概的意思，选中AD1，然后在里面输入公式，完事后选中AD1，复制，再选中AD1到AD11000，黏贴
+ '
+  Sub 按钮4_Click()
+    Range("AD1").Select
+    ActiveCell.FormulaR1C1 = _
+        "=OFFSET(R3C[-29],TRUNC((ROW()-1)/22),MOD(ROW()-1,22))"
+    Range("AD1").Select
+    Selection.Copy
+    Range("AD1:AD11000").Select
+    ActiveSheet.Paste
+  End Sub
